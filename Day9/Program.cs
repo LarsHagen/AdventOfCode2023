98435a418dc77d6c80c8107f77cf9785077fd6cf
Lars Hagen <mail@laitch.com>
2023-12-09 10:59:23 +0100

    Day 9

---
+++ Day9/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Day9;
+
+var lines = File.ReadAllLines("input.txt");
+
+int sumPart1 = 0;
+int sumPart2 = 0;
+
+foreach (var line in lines)
+{
+    Reading reading = new Reading(line);
+    sumPart1 += reading.Extrapolate();
+    sumPart2 += reading.ExtrapolateBack();
+}
+
+Console.WriteLine("Part 1: " + sumPart1);
+Console.WriteLine("Part 2: " + sumPart2);
---
+++ Day9/Reading.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Day9
+{
+    internal class Reading
+    {
+        public List<List<int>> sequences = new();
+
+        public Reading(string input)
+        {
+            var initialSequence = input.Split(" ").Select(v => int.Parse(v)).ToList();
+            sequences.Add(initialSequence);
+
+            GenerateLowerRecursive(initialSequence);
+        }
+
+        private void GenerateLowerRecursive(List<int> current)
+        {
+            bool done = true;
+
+            List<int> lower = new List<int>();
+
+            for (int i = 0; i < current.Count - 1; i++)
+            {
+                var difference = current[i + 1] - current[i];
+
+                if (difference != 0)
+                    done = false;
+
+                lower.Add(difference);
+            }
+
+            sequences.Add(lower);
+
+            if (!done)
+                GenerateLowerRecursive(lower);
+        }
+
+        public int Extrapolate()
+        {
+            sequences.Last().Add(0);
+
+            for (int i = sequences.Count - 2; i >= 0; i--)
+            {
+                sequences[i].Add(sequences[i].Last() + sequences[i + 1].Last());
+            }
+
+            return sequences[0].Last();
+        }
+
+        public int ExtrapolateBack()
+        {
+            sequences.Last().Insert(0, 0);
+
+
+            for (int i = sequences.Count - 2; i >= 0; i--)
+            {
+                sequences[i].Insert(0, sequences[i].First() - sequences[i + 1].First());
+            }
+
+            return sequences[0].First();
+        }
+    }
+}
